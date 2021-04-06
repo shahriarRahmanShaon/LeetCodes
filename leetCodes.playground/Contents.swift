f87946273b48f2827a8f923b046093996a16fbdf
shahriarRahmanShaon <shaon463@gmail.com>
2021-04-06 15:23:25 +0600

    leetcode problem solutions in swift 5.1

---
+++ leetCodes.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+//import Foundation
+//func twoSum (_ nums: [Int], _ target: Int) -> [Int] {
+//    var result = [Int]()
+//    for index1 in 0..<nums.count-1 {
+//        for index2 in 1..<nums.count {
+//            if nums[index1] == target - nums[index2] {
+//                result = [index1, index2]
+//            }
+//        }
+//    }
+//    return result
+//}
+
+//MARK:- leetcode1
+
+
+
